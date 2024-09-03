762d0f6482a3038b7ac05f5cdc200e43cbf1fd86
Will Gregory <willgregory97@gmail.com>
2024-09-03 10:48:15 -0400

    Fixed paths

--- sync.sh
+++ sync.sh
@@ -14,8 +14,8 @@ echo $$ > /tmp/sync.pid
 if test "$(rclone ls "$SYNC_SRC" $RCLONE_OPTS)"; then
   # the source directory is not empty
   # it can be synced without clear data loss
-  echo "INFO: Starting rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS"
-  rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
+  echo "INFO: Starting rclone sync \"$SYNC_SRC\" \"$SYNC_DEST\" $RCLONE_OPTS $SYNC_OPTS"
+  rclone sync "$SYNC_SRC" "$SYNC_DEST" $RCLONE_OPTS $SYNC_OPTS
 
   if [ -z "$CHECK_URL" ]
   then
